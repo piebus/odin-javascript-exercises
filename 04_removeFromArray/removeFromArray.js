1ea5b800949888c67c4ffcc0eff0c1a49f472773
piebus <piers.busser@gmail.com>
2024-10-03 16:25:20 -0700

    complete removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...badItems) {
+    let items = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (!(badItems.includes(arr[i]))) {
+             items.push(arr[i])
+        }
+    }
+    return items;
 };
 
 // Do not edit below this line
