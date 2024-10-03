38f0acbac8ecbc6d24f26df9de9b7e7bddf078bf
piebus <piers.busser@gmail.com>
2024-10-03 16:41:14 -0700

    complete sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {return "ERROR";}
+    if (num1 < 0 || num2 < 0) {return "ERROR";}
 
+    // ensure num1 is the smaller value
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    var sum = 0;
+    while (num1 <= num2) {
+        sum += num1;
+        num1++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
