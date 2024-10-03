9b2dd1e553fd8ea699d3aaba03fbe840d0e4a30a
piebus <piers.busser@gmail.com>
2024-10-03 15:24:23 -0700

    copmlete repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {return "ERROR";}
+    let repeated = "";
+    for (var i = 0; i < num; i++) {
+        repeated += str
+    }
+    return repeated;
 };
 
 // Do not edit below this line
