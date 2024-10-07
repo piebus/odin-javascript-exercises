64fd0e69f9747c237329f0ee4f57eb65ea1243fc
piebus <piers.busser@gmail.com>
2024-10-07 13:05:38 -0700

    Add temperature conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+  const unrounded = (tempInFarenheit -32) * 5 / 9;
+  return Math.round(unrounded * 10) /10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  const unrounded = tempInCelcius * 9 / 5 + 32;
+  return Math.round(unrounded * 10) /10
 };
 
 // Do not edit below this line
