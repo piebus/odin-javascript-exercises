31cc477c816c9a948295cb3b0c23716b8ade0e03
piebus <piers.busser@gmail.com>
2024-10-07 11:56:40 -0700

    Complete leapYear calculator function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        // A year divisible by 4 is a leap year
+        // but if a year is divisible by 100 it is only a leap year
+        // if it is also divisible by 400
+        if (year % 100 == 0) {return year % 400 == 0;}
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
