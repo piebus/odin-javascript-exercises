640808ccc25b60b9ecbc058600e15aac030e7789
piebus <piers.busser@gmail.com>
2024-10-24 12:16:39 -0700

    Finish findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   const oldie = people.reduce((oldest, currentPerson) => {
+        return getAge(oldest) > getAge(currentPerson) ? oldest : currentPerson;
+   });
+   return oldie;
 };
+
+function getAge(person) {
+    deathYear = person.yearOfDeath ?? new Date().getFullYear();
+    return deathYear - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
