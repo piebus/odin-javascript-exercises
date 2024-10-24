96a67b967fdb022b321de571ddd5671623cc4160
piebus <piers.busser@gmail.com>
2024-10-24 11:19:39 -0700

    Complete Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const num = Number(input);
+    if (Number.isNaN(num)) {return "OOPS";}
+    if (num < 0) {return "OOPS";}
+    return fibHelper(num);
 };
+
+function fibHelper(number) {
+    if (number == 0) {return 0;}
+    if (number == 1) {return 1;}
+    return fibHelper(number-1) + fibHelper(number-2);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
