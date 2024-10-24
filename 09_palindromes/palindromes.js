f8716be059ebd4131e439eb673368f62b8df6ffc
piebus <piers.busser@gmail.com>
2024-10-24 11:08:35 -0700

    Complete palindrome method

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const consideredCharacters = "abcdefghijklmnopqrstuvwxyz1234567890"
+
+    str = str.toLowerCase();
+    const forwardsArray = str.split("")
+        .filter((char) => consideredCharacters.includes(char));
+
+    //make a copy of the array because .reverse() modifies the original array
+    var backwardsArray = forwardsArray.slice();
+    backwardsArray.reverse();
+
+    return forwardsArray.join("") == backwardsArray.join("");
 };
 
 // Do not edit below this line
